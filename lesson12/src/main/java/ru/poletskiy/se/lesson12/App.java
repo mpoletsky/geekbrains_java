61572d7e7baddc27cd3aa44669755726922b19de
Poletskiy Maxim <mpoletsky@mail.ru>
2018-10-21 15:07:21 +0600

    Выполнил задание 1

--- lesson12/src/main/java/ru/poletskiy/se/lesson12/App.java
+++ lesson12/src/main/java/ru/poletskiy/se/lesson12/App.java
@@ -1,13 +1,18 @@
 package ru.poletskiy.se.lesson12;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.util.Arrays;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // Задание 1
+        int[] task1IntArray = new int[]{1,1,0,1,0,0,1,1,0,0,0,1};
+        System.out.println("Задание 1. Исходный массив: " + Arrays.toString(task1IntArray));
+        for (int i = 0; i < task1IntArray.length; i++) {
+            if (task1IntArray[i] == 0) task1IntArray[i] = 1;
+            else task1IntArray[i] = 0;
+        }
+        System.out.println("Задание 1. Результат:       " + Arrays.toString(task1IntArray));
     }
 }
